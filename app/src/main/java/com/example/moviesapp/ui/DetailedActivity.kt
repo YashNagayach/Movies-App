ae283672c59a726674ede92a92b2511504a75e8e
YashNagayach <yash.nagayach@dunzo.in>
2023-11-26 19:04:53 +0530

    Details Screen added

--- app/src/main/java/com/example/moviesapp/ui/DetailedActivity.kt
+++ app/src/main/java/com/example/moviesapp/ui/DetailedActivity.kt
@@ -4,18 +4,29 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.bumptech.glide.Glide
 import com.example.moviesapp.R
+import com.example.moviesapp.databinding.ActivityDetailedBinding
 import com.example.moviesapp.model.Movie
 
 class DetailedActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityDetailedBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detailed)
+        binding = ActivityDetailedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val movieDesc = intent.getStringExtra("movie")
-        val imageUrl = intent.getStringExtra("image")
+        val imageUrl = "https://image.tmdb.org/t/p/w185/${intent.getStringExtra("image")}"
 
+        Glide.with(this)
+            .load(imageUrl)
+            .into(binding.image)
 
+        binding.title.text = movieDesc
 
     }
 
